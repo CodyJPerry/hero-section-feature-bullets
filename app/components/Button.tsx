ae2a50ac73be40f1d0c0db253bc53863eeeaa4c4
Cody Perry <perrysdevelopment@gmail.com>
2024-06-06 00:39:55 -0400

    Update styling and abstract the buttons into their own component file to make them more reusable

---
+++ app/components/Button.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+interface ButtonProps {
+  children: React.ReactNode;
+  className?: string;
+}
+
+const Button: React.FC<ButtonProps> = ({ children, className }) => {
+  return (
+    <button
+      className={`text-base md:text-lg font-medium px-5 py-3 md:px-10 md:py-4 md:w-[213px] border-[0.5px] rounded grow md:grow-0 shadow focus:outline-none focus:ring-4 focus:ring-offset-7 focus:ring-indigo-100 focus:rounded-sm focus:bg-[#FAFAFA] disabled:cursor-not-allowed disabled:bg-[#F5F5F5] disabled:text-[#A3A3A3] ${className}`}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 import Image from 'next/image';
 import { RiCheckFill } from 'react-icons/ri';
+import Button from './components/Button';
 
 export default function Home() {
   return (
     <main className='m-4' role='main'>
       {/* Base Card */}
-      <section className='bg-white py-14 lg:py-24 shadow-sm md:shadow-md lg:shadow-lg rounded'>
-        <div className='rounded px-3 py-12 lg:flex lg:px-24 lg:py-0 lg:gap-8'>
+      <section className='bg-white py-14 lg:py-24 shadow-sm md:shadow-md lg:shadow-lg rounded max-w-screen-xl mx-auto'>
+        <div className='rounded px-3 py-12 lg:flex lg:px-24 lg:py-0 lg:gap-8 md:flex md:justify-between md:items-center'>
           {/* Content */}
           <div>
             {/* Heading */}
@@ -30,12 +31,12 @@ export default function Home() {
             </ul>
             {/* Buttons */}
             <div className='flex gap-4 md:gap-8 mb-12 md:mb-8'>
-              <button className='text-base md:text-lg text-neutral-900 font-medium px-5 py-3 md:px-10 md:py-4 md:w-[213px] border-[0.5px] bg-white rounded grow md:grow-0 shadow hover:bg-[#FAFAFA] focus:outline-none focus:ring-4 focus:ring-offset-7 focus:ring-indigo-100 focus:rounded-sm focus:bg-[#FAFAFA] disabled:cursor-not-allowed disabled:bg-[#F5F5F5] disabled:text-[#A3A3A3] order-2'>
+              <Button className='text-neutral-900 bg-white hover:bg-[#FAFAFA] disabled:bg-[#F5F5F5] disabled:text-[#A3A3A3] order-2 md:order-1'>
                 Learn more
-              </button>
-              <button className='text-base md:text-lg text-white font-medium bg-indigo-700 px-5 py-3 md:px-10 md:py-4 md:w-[213px] rounded grow md:grow-0 shadow hover:bg-[#3730A3] focus:outline-none focus:ring-4 focus:ring-offset-7 focus:ring-indigo-100 focus:rounded-sm disabled:cursor-not-allowed disabled:bg-[#F5F5F5] disabled:text-[#A3A3A3]'>
+              </Button>
+              <Button className='text-white bg-indigo-700 hover:bg-[#3730A3] focus:ring-indigo-100 focus:bg-[#3730A3] order-1 md:order-2'>
                 See pricing
-              </button>
+              </Button>
             </div>
           </div>
           {/* Image */}
